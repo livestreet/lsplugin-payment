0da4df3470710ebf4201d9b08cea95eb78792191
Mzhelskiy Maxim <rus.engine@gmail.com>
2013-05-18 10:22:12 +0700

    Поддержка редиректов через Router::Action()

--- classes/actions/ActionPayment.class.php
+++ classes/actions/ActionPayment.class.php
@@ -16,7 +16,7 @@
 */
 
 class PluginPayment_ActionPayment extends ActionPlugin {
-	
+
 	public function Init() {
 		$this->Viewer_AppendStyle(Plugin::GetTemplateWebPath(__CLASS__).'css/style.css');
 		$this->Viewer_AppendScript(Plugin::GetTemplateWebPath(__CLASS__).'js/main.js');
@@ -28,7 +28,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 	protected function RegisterEvent() {
 		$this->AddEvent('ajax-processing','EventAjaxProcessing');
 		$this->AddEventPreg('/^\d+$/i','EventIndex');
-		
+
 		$this->AddEventPreg('/^wm$/i','/^result$/i','EventWmResult');
 		$this->AddEventPreg('/^wm$/i','/^success$/i','EventWmSuccess');
 		$this->AddEventPreg('/^wm$/i','/^fail$/i','EventWmFail');
@@ -36,26 +36,26 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 		$this->AddEventPreg('/^master$/i','/^result$/i','EventMasterResult');
 		$this->AddEventPreg('/^master$/i','/^success$/i','EventMasterSuccess');
 		$this->AddEventPreg('/^master$/i','/^fail$/i','EventMasterFail');
-		
+
 		$this->AddEventPreg('/^liqpay$/i','/^result$/i','EventLiqpayResult');
-		
+
 		$this->AddEventPreg('/^robox$/i','/^result$/i','EventRoboxResult');
 		$this->AddEventPreg('/^robox$/i','/^success$/i','EventRoboxSuccess');
 		$this->AddEventPreg('/^robox$/i','/^fail$/i','EventRoboxFail');
-		
+
 		$this->AddEventPreg('/^paypro$/i','/^notify$/i','EventPayproNotify');
 	}
 
 
 	/**********************************************************************************
-	************************ РЕАЛИЗАЦИЯ ЭКШЕНА ***************************************
-	**********************************************************************************
-	*/
-	
-	
+	 ************************ РЕАЛИЗАЦИЯ ЭКШЕНА ***************************************
+	 **********************************************************************************
+	 */
+
+
 	protected function EventAjaxProcessing() {
 		$this->Viewer_SetResponseAjax('json');
-		
+
 		/**
 		 * Проверка платежа
 		 */
@@ -63,7 +63,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			$this->Message_AddErrorSingle($this->Lang_Get('plugin.payment.error_payment_id'),$this->Lang_Get('error'));
 			return;
 		}
-		
+
 		/**
 		 * Проверка состония платежа
 		 */
@@ -71,7 +71,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			$this->Message_AddErrorSingle($this->Lang_Get('plugin.payment.error_state'),$this->Lang_Get('error'));
 			return;
 		}
-		
+
 		/**
 		 * Проверка связи с объектом покупки
 		 */
@@ -79,7 +79,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			$this->Message_AddErrorSingle($this->Lang_Get('plugin.payment.error_target'),$this->Lang_Get('error'));
 			return;
 		}
-		
+
 		/**
 		 * Проверяем разрешение на платеж
 		 */
@@ -87,7 +87,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			$this->Message_AddErrorSingle($this->Lang_Get('plugin.payment.error_payment_not_access'),$this->Lang_Get('error'));
 			return;
 		}
-		
+
 		/**
 		 * Определяем допустимые типы оплаты в зависимоти от валюты
 		 */
@@ -100,21 +100,21 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 		$oPayment->setType($sType);
 		$this->PluginPayment_Payment_UpdatePayment($oPayment);
 		/**
-		 * 
+		 *
 		 * Формируем описание покупки для платежной системы, добавляем в конец номер платежа (будет проще решать возможные проблемы с платежами)
 		 */
 		$sDescription=$oTarget->getTargetPaymentDescription().', №'.$oPayment->getId();
-		
+
 		$oViewerLocal=$this->Viewer_GetLocalViewer();
 
-		
-		if ($oPayment->getType()==PluginPayment_ModulePayment::PAYMENT_TYPE_WM) {			
+
+		if ($oPayment->getType()==PluginPayment_ModulePayment::PAYMENT_TYPE_WM) {
 			$oViewerLocal->Assign('LMI_PAYMENT_AMOUNT',$oPayment->getSum());
 			$oViewerLocal->Assign('LMI_PAYMENT_DESC',$sDescription);
 			$oViewerLocal->Assign('LMI_PAYMENT_DESC_BASE64',base64_encode($sDescription));
 			$oViewerLocal->Assign('LMI_PAYMENT_NO',$oPayment->getId());
 			$oViewerLocal->Assign('LMI_PAYEE_PURSE',$this->PluginPayment_Payment_GetWmPurseByCurrency($oPayment->getCurrencyId()));
-			
+
 			$oViewerLocal->Assign('LMI_SIM_MODE',0);
 			$oViewerLocal->Assign('LMI_RESULT_URL',Router::getPath('payment').'wm/result/');
 			$oViewerLocal->Assign('LMI_SUCCESS_URL',Router::getPath('payment').'wm/success/');
@@ -151,32 +151,32 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 				<pay_way>card</pay_way> 
 			</request>";
 
-			$sSig = base64_encode(sha1(Config::Get('plugin.payment.liqpay.signature').$sXml.Config::Get('plugin.payment.liqpay.signature'),1));			
+			$sSig = base64_encode(sha1(Config::Get('plugin.payment.liqpay.signature').$sXml.Config::Get('plugin.payment.liqpay.signature'),1));
 			$oViewerLocal->Assign('LIQPAY_OPERATION_XML',base64_encode($sXml));
 			$oViewerLocal->Assign('LIQPAY_SIGNATURE',$sSig);
 			$this->Viewer_AssignAjax('sFormText',$oViewerLocal->Fetch(Plugin::GetTemplatePath(__CLASS__)."payment.liqpay.tpl"));
-		} elseif ($oPayment->getType()==PluginPayment_ModulePayment::PAYMENT_TYPE_PAYPRO) {	
+		} elseif ($oPayment->getType()==PluginPayment_ModulePayment::PAYMENT_TYPE_PAYPRO) {
 			$oViewerLocal->Assign('PAYPRO_PRODUCTS',Config::Get('plugin.payment.paypro.products'));
 			$oViewerLocal->Assign('PAYPRO_CUSTOMFIELD1',$oPayment->getKey());
 			$oViewerLocal->Assign('PAYPRO_HASH',$this->PluginPayment_Payment_PayProGetHash($oPayment->getSum(),$sDescription));
-			
+
 			$this->Viewer_AssignAjax('sFormText',$oViewerLocal->Fetch(Plugin::GetTemplatePath(__CLASS__)."payment.paypro.tpl"));
-		} elseif ($oPayment->getType()==PluginPayment_ModulePayment::PAYMENT_TYPE_ROBOX) {	
+		} elseif ($oPayment->getType()==PluginPayment_ModulePayment::PAYMENT_TYPE_ROBOX) {
 			$oViewerLocal->Assign('MrchLogin',Config::Get('plugin.payment.robox.login'));
 			$oViewerLocal->Assign('OutSum',$oPayment->getSum());
 			$oViewerLocal->Assign('InvId',$oPayment->getId());
 			$oViewerLocal->Assign('Desc',$sDescription);
 			$oViewerLocal->Assign('SignatureValue',md5(Config::Get('plugin.payment.robox.login').":{$oPayment->getSum()}:{$oPayment->getId()}:".Config::Get('plugin.payment.robox.password_1').":shp_key={$oPayment->getKey()}"));
-			$oViewerLocal->Assign('IncCurrLabel','PCR');			
+			$oViewerLocal->Assign('IncCurrLabel','PCR');
 			$oViewerLocal->Assign('shp_key',$oPayment->getKey());
-			
+
 			$this->Viewer_AssignAjax('sFormText',$oViewerLocal->Fetch(Plugin::GetTemplatePath(__CLASS__)."payment.robox.tpl"));
 		} else {
 			$this->Message_AddErrorSingle($this->Lang_Get('plugin.payment.error_payment_available_type'),$this->Lang_Get('error'));
 			return;
 		}
 	}
-	
+
 	/**
 	 * Страница оформления платежа
 	 * Необходимо передать GET параметр payment_id - номер счета(платежа), который нужно оплатить
@@ -184,7 +184,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 	 * @return unknown
 	 */
 	protected function EventIndex() {
-		
+
 		/**
 		 * Проверка платежа
 		 */
@@ -192,7 +192,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			$this->Message_AddErrorSingle($this->Lang_Get('plugin.payment.error_payment_id'),$this->Lang_Get('error'));
 			return Router::Action('error');
 		}
-		
+
 		/**
 		 * Проверка состония платежа
 		 */
@@ -200,7 +200,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			$this->Message_AddErrorSingle($this->Lang_Get('plugin.payment.error_state'),$this->Lang_Get('error'));
 			return Router::Action('error');
 		}
-		
+
 		/**
 		 * Проверка связи с объектом покупки
 		 */
@@ -208,7 +208,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			$this->Message_AddErrorSingle($this->Lang_Get('plugin.payment.error_target'),$this->Lang_Get('error'));
 			return Router::Action('error');
 		}
-		
+
 		/**
 		 * Проверяем разрешение на платеж
 		 */
@@ -216,7 +216,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			$this->Message_AddErrorSingle($this->Lang_Get('plugin.payment.error_payment_not_access'),$this->Lang_Get('error'));
 			return Router::Action('error');
 		}
-		
+
 		/**
 		 * Определяем допустимые типы оплаты в зависимоти от валюты
 		 */
@@ -225,8 +225,8 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			$this->Message_AddErrorSingle($this->Lang_Get('plugin.payment.error_payment_available_type'),$this->Lang_Get('error'));
 			return Router::Action('error');
 		}
-		
-		
+
+
 		$this->Viewer_Assign('oPayment',$oPayment);
 		$this->Viewer_Assign('oPaymentTarget',$oTarget);
 		$this->Viewer_Assign('aPaymentTypeAvailable',$aTypeAvailable);
@@ -237,37 +237,37 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 	 * Платеж завершен успешно, выполняется по инициативе сервера платежной системы сразу после покупки.
 	 * Именно в этом методе нужно фиксировать факт оплаты
 	 * Редиректы в данном методе не имеют смысла
-	 * 
+	 *
 	 * @param unknown_type $oPayment
 	 */
 	protected function MakePaymentSuccess($oPayment) {
 		$this->PluginPayment_Payment_MakePaymentSuccess($oPayment);
 	}
-	
+
 	/**
 	 * Информирование об успешном платеже - выполняется в момент редиректа пользователя после оплаты
-	 * 
+	 *
 	 * @param unknown_type $oPayment
 	 */
 	protected function ProcessPaymentSuccess($oPayment) {
-		$this->PluginPayment_Payment_ProcessPaymentSuccess($oPayment);
+		return $this->PluginPayment_Payment_ProcessPaymentSuccess($oPayment);
 	}
-	
+
 	/**
 	 * Информирование об несостоявшемся платеже - выполняется в момент редиректа пользователя после попытки оплаты
-	 * 
+	 *
 	 * @param unknown_type $oPayment
 	 */
 	protected function ProcessPaymentFail($oPayment) {
-		$this->PluginPayment_Payment_ProcessPaymentFail($oPayment);
+		return $this->PluginPayment_Payment_ProcessPaymentFail($oPayment);
 	}
-	
+
 	/**
 	 * Обработка запроса Webmoney
 	 * Совершение платежа
 	 */
-	protected function EventWmResult() {		
-		if (getRequest('LMI_PREREQUEST')==1) {		
+	protected function EventWmResult() {
+		if (getRequest('LMI_PREREQUEST')==1) {
 			$iError=$this->PluginPayment_Payment_PreResultWm();
 			if ($iError==0) {
 				echo('YES');
@@ -281,7 +281,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 		}
 		exit();
 	}
-	
+
 	/**
 	 * Обработка запроса Webmoney
 	 * Редирект после успешной покупки
@@ -294,16 +294,22 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			return ;
 		}
 		$this->Viewer_Assign('oPayment',$oPayment);
-		
+
 		if ($iError===0) {
-			$this->ProcessPaymentSuccess($oPayment);
+			$mRes=$this->ProcessPaymentSuccess($oPayment);
+			if ($mRes=='next') {
+				return $mRes;
+			}
 			$this->SetTemplateAction('success');
 		} else {
-			$this->ProcessPaymentFail($oPayment);
+			$mRes=$this->ProcessPaymentFail($oPayment);
+			if ($mRes=='next') {
+				return $mRes;
+			}
 			$this->SetTemplateAction('fail');
 		}
 	}
-	
+
 	/**
 	 * Обработка запроса Webmoney
 	 * Редирект после незавершенного платежа
@@ -311,13 +317,16 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 	protected function EventWmFail() {
 		$iError=$this->PluginPayment_Payment_FailWm();
 		$this->SetTemplateAction('fail');
-		
+
 		$oPayment=$this->PluginPayment_Payment_GetPaymentCurrent();
-		if (!$oPayment) {			
+		if (!$oPayment) {
 			return ;
 		}
 		$this->Viewer_Assign('oPayment',$oPayment);
-		$this->ProcessPaymentFail($oPayment);
+		$mRes=$this->ProcessPaymentFail($oPayment);
+		if ($mRes=='next') {
+			return $mRes;
+		}
 	}
 
 	/**
@@ -354,10 +363,16 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 		$this->Viewer_Assign('oPayment',$oPayment);
 
 		if ($iError===0) {
-			$this->ProcessPaymentSuccess($oPayment);
+			$mRes=$this->ProcessPaymentSuccess($oPayment);
+			if ($mRes=='next') {
+				return $mRes;
+			}
 			$this->SetTemplateAction('success');
 		} else {
-			$this->ProcessPaymentFail($oPayment);
+			$mRes=$this->ProcessPaymentFail($oPayment);
+			if ($mRes=='next') {
+				return $mRes;
+			}
 			$this->SetTemplateAction('fail');
 		}
 	}
@@ -375,7 +390,10 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			return ;
 		}
 		$this->Viewer_Assign('oPayment',$oPayment);
-		$this->ProcessPaymentFail($oPayment);
+		$mRes=$this->ProcessPaymentFail($oPayment);
+		if ($mRes=='next') {
+			return $mRes;
+		}
 	}
 
 	/**
@@ -389,24 +407,30 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			return ;
 		}
 		$this->Viewer_Assign('oPayment',$oPayment);
-		
+
 		if (is_array($result)) {
 			if ($result[1]=='success') {
 				// проводим платеж
 				$this->MakePaymentSuccess($oPayment);
-				
+
 				if ($result[0]=='server_url') {
 					exit();
 				} else {
 					$this->SetTemplateAction('success');
-					$this->ProcessPaymentSuccess($oPayment);
+					$mRes=$this->ProcessPaymentSuccess($oPayment);
+					if ($mRes=='next') {
+						return $mRes;
+					}
 				}
 			} elseif ($result[1]=='success_repeat') {
 				if ($result[0]=='server_url') {
 					exit();
 				} else {
 					$this->SetTemplateAction('success');
-					$this->ProcessPaymentSuccess($oPayment);
+					$mRes=$this->ProcessPaymentSuccess($oPayment);
+					if ($mRes=='next') {
+						return $mRes;
+					}
 				}
 			} elseif ($result[1]=='wait_secure') {
 				$this->SetTemplateAction('wait');
@@ -423,27 +447,39 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 					exit();
 				} else {
 					$this->SetTemplateAction('fail');
-					$this->ProcessPaymentFail($oPayment);
+					$mRes=$this->ProcessPaymentFail($oPayment);
+					if ($mRes=='next') {
+						return $mRes;
+					}
 				}
 			} elseif ($result[1]=='failure_repeat') {
 				if ($result[0]=='server_url') {
 					exit();
 				} else {
 					$this->SetTemplateAction('fail');
-					$this->ProcessPaymentFail($oPayment);
+					$mRes=$this->ProcessPaymentFail($oPayment);
+					if ($mRes=='next') {
+						return $mRes;
+					}
 				}
 			} else {
 				$this->SetTemplateAction('fail');
-				$this->ProcessPaymentFail($oPayment);
+				$mRes=$this->ProcessPaymentFail($oPayment);
+				if ($mRes=='next') {
+					return $mRes;
+				}
 				return ;
 			}
 		} else {
 			$this->SetTemplateAction('fail');
-			$this->ProcessPaymentFail($oPayment);
+			$mRes=$this->ProcessPaymentFail($oPayment);
+			if ($mRes=='next') {
+				return $mRes;
+			}
 			return ;
 		}
 	}
-	
+
 	/**
 	 * Обработка запроса Робокассы
 	 * Совершение платежа
@@ -456,14 +492,14 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 				$this->SetTemplateAction('fail');
 				exit();
 			}
-			
+
 			// проводим платеж
 			$this->MakePaymentSuccess($oPayment);
 			echo('OK'.$oPayment->getId());
 		}
 		exit();
 	}
-	
+
 	/**
 	 * Обработка запроса Робокассы
 	 * Редирект после успешной покупки
@@ -476,16 +512,22 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			return ;
 		}
 		$this->Viewer_Assign('oPayment',$oPayment);
-			
-		if ($iError===0) {			
-			$this->ProcessPaymentSuccess($oPayment);
+
+		if ($iError===0) {
+			$mRes=$this->ProcessPaymentSuccess($oPayment);
+			if ($mRes=='next') {
+				return $mRes;
+			}
 			$this->SetTemplateAction('success');
-		} else {			
-			$this->ProcessPaymentFail($oPayment);
+		} else {
+			$mRes=$this->ProcessPaymentFail($oPayment);
+			if ($mRes=='next') {
+				return $mRes;
+			}
 			$this->SetTemplateAction('fail');
-		}		
+		}
 	}
-	
+
 	/**
 	 * Обработка запроса Робокассы
 	 * Редирект после незавершенного платежа
@@ -498,9 +540,12 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			return ;
 		}
 		$this->Viewer_Assign('oPayment',$oPayment);
-		$this->ProcessPaymentFail($oPayment);
+		$mRes=$this->ProcessPaymentFail($oPayment);
+		if ($mRes=='next') {
+			return $mRes;
+		}
 	}
-	
+
 	/**
 	 * Обработка запроса от PayPro (оплата paypal)
 	 */
@@ -510,7 +555,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 		if (!$oPayment) {
 			exit();
 		}
-		
+
 		/**
 		 * Платеж прошел успешно
 		 */
@@ -518,7 +563,7 @@ class PluginPayment_ActionPayment extends ActionPlugin {
 			// проводим платеж
 			$this->MakePaymentSuccess($oPayment);
 		} else {
-			
+
 		}
 		exit();
 	}
